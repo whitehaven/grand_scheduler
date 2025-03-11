62ddf8ff2858eb9f26295a4c2f8b712368332164
Alex Crist <accrist@gmail.com>
2025-03-10 23:08:43 -0700

    reactivate prior experiments, non-maximizing runs fast but multi-year requirements challenging

---
+++ opre_tools.py
@@ -0,0 +1,34 @@
+def negated_bounded_span(works, start, length):
+    """Filters an isolated sub-sequence of variables assined to True.
+
+    Extract the span of Boolean variables [start, start + length), negate them,
+    and if there is variables to the left/right of this span, surround the span by
+    them in non negated form.
+
+    Args:
+      works: a list of variables to extract the span from.
+      start: the start to the span.
+      length: the length of the span.
+
+    Returns:
+      a list of variables which conjunction will be false if the sub-list is
+      assigned to True, and correctly bounded by variables assigned to False,
+      or by the start or end of works.
+    """
+    sequence = []
+    # Left border (start of works, or works[start - 1])
+    if start > 0:
+        sequence.append(works[start - 1])
+    for i in range(length):
+        sequence.append(works[start + i].Not())
+    # Right border (end of works or works[start + length])
+    if start + length < len(works):
+        sequence.append(works[start + length])
+    # print(f"seq = {sequence}")
+    return sequence
+
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    print(x)
+    pd.reset_option('display.max_rows')
+    return None
